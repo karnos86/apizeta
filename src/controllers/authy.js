840d736c4f59eb89dabd812d9ee1698dc2324924
ronald <ronald.penaloza08@gmail.com>
2019-04-09 12:25:08 -0430

    soluccion

--- src/controllers/authy.js
+++ src/controllers/authy.js
@@ -85,11 +85,11 @@ module.exports={
       let cookie = req.headers["authorization"].split(" ");
       let data = await asyn_request('https://zetatijuana.com/api/user/validate_auth_cookie/?cookie='+cookie[1],{method: 'GET'})
       res.json(JSON.parse(data.body))
-      // if(info.status == 'ok' && info.valid == true){
-      //   next()
-      // }else{
-      //   res.status(401).json({message: 'no autorizado'})
-      // }
+      if(info.status == 'ok' && info.valid == true){
+        next()
+      }else{
+        res.status(401).json({message: 'no autorizado'})
+      }
     }catch(error){
       console.log(error)
       res.status(500).json(error);
