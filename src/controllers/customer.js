4770158d8a7f72ff2fe904cdae4d2b138288eabf
ronald <ronald.penaloza08@gmail.com>
2019-04-12 21:05:16 -0430

    avance

--- src/controllers/customer.js
+++ src/controllers/customer.js
@@ -17,7 +17,7 @@ module.exports={
            console.log(nonce);
            if(nonce.status=="ok"){
                console.log(process.env.CNAME_EXTERNAL+'/api/user/register/?username='+data.username+'&email='+data.email+'&nonce='+nonce.nonce+'&display_name='+data.name+'&notify=both&user_pass='+data.password)
-                var result = await asyn_request(process.env.CNAME_EXTERNAL+'/api/user/register/?username='+data.username+'&email='+data.email+'&nonce='+nonce.nonce+'&display_name='+data.name+'&notify=both&user_pass='+data.password,{method: 'GET', cookieJar: true});
+                var result = await asyn_request(process.env.CNAME_EXTERNAL+'/api/user/register/?username='+data.username+'&email='+data.email+'&nonce='+nonce.nonce+'&display_name='+data.name+'&notify=both&user_pass='+data.password,{method: 'GET'});
                 res.json(result)
                 wordpress = JSON.parse(result.body);
                 console.log(wordpress)
