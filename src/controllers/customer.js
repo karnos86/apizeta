4bfaf5e6a9bbee26256668ec86493013ad3662c1
ronald <ronald.penaloza08@gmail.com>
2019-04-12 20:02:05 -0430

    ver

--- src/controllers/customer.js
+++ src/controllers/customer.js
@@ -16,6 +16,7 @@ module.exports={
            let nonce = JSON.parse(generar_nonce.body);
            console.log(nonce);
            if(nonce.status=="ok"){
+               console.log(process.env.CNAME_EXTERNAL+'/api/user/register/?username='+data.username+'&email='+data.email+'&nonce='+nonce.nonce+'&display_name='+data.name+'&notify=both&user_pass='+data.password)
                 var result = await asyn_request(process.env.CNAME_EXTERNAL+'/api/user/register/?username='+data.username+'&email='+data.email+'&nonce='+nonce.nonce+'&display_name='+data.name+'&notify=both&user_pass='+data.password,{method: 'GET', cookieJar: true});
                 console.log(result)
                 wordpress = JSON.parse(result.body);
