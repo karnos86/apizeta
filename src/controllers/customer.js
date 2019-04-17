82b31e00a95689cb07d99deb81c8582631979f34
ronald <ronald.penaloza08@gmail.com>
2019-04-17 16:59:38 -0430

    tdc personal

--- src/controllers/customer.js
+++ src/controllers/customer.js
@@ -101,11 +101,8 @@ module.exports={
            console.log(data)
            let conekt = await conekta.Customer.create(data.customer_info[0]);
            console.log(conekt)
-           console.log(conekt._id);
            await Customer.create({idWordPress: data.wordpress, idConekt: conekt._id, email:data.customer_info[0].email});
            data["customer_info"] = {customer_id: conekt._id};
-           delete data["wordpress"];
-           let orden = await conekta.Order.create(data);
            res.json(orden["charges"]._json);
 
         }catch(error){
