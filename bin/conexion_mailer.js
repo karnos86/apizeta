5be6219e82aa7b4458b6ba6e357dc8090e12566f
ronald <ronald.penaloza08@gmail.com>
2019-04-18 04:43:48 -0430

    mailer

--- bin/conexion_mailer.js
+++ bin/conexion_mailer.js
@@ -2,10 +2,10 @@ var nodemailer = require('nodemailer');
 var smtpTransport = require("nodemailer-smtp-transport");
 
 module.exports = nodemailer.createTransport(smtpTransport({
-	pool: true,
+	// pool: true,
     host : process.env.HOST_MAIL,
     port:  process.env.PORT_MAIL,
-    secure: false,
+    // secure: false,
     auth : {
         user : process.env.USER_MAIL,
         pass : process.env.PASS_MAIL
