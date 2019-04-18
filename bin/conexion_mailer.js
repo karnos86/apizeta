5c7ae9e8d94fc459b9aea2e318d605ecc65ba194
ronald <ronald.penaloza08@gmail.com>
2019-04-18 05:17:32 -0430

    mail

--- bin/conexion_mailer.js
+++ bin/conexion_mailer.js
@@ -6,9 +6,9 @@ module.exports = nodemailer.createTransport(smtpTransport({
     host : process.env.HOST_MAIL,
     port:  process.env.PORT_MAIL,
     // secure: false,
-    auth : {
-        user : process.env.USER_MAIL,
-        pass : process.env.PASS_MAIL
-    }
+    // auth : {
+    //     user : process.env.USER_MAIL,
+    //     pass : process.env.PASS_MAIL
+    // }
    
 }));
