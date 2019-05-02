6d3a5f95718adceee1ee9173681e109540f08d4a
ronald <ronald.penaloza08@gmail.com>
2019-05-02 18:01:40 -0430

    nuevo serve

--- src/plugins/headers.js
+++ src/plugins/headers.js
@@ -9,13 +9,13 @@ module.exports={
         headers:{
             'Content-Type': 'application/json ',
             'charset':'UTF-8',
-            'Authorization': 'Bearer AdminTest|1557462690|KURwQrTWnbMLFGz7tbiQfMhomJB4aLGA79ZBk2MVRAY|f3f36aa67c608ac5f734da8389a678f5361db1a841d9ca3f6a1f4353dcf2d68c' 
+            'Authorization': 'Bearer '+localStorage.cookie
         }
     },
     authFile:{
         headers:{
             'Content-Type': 'multipart/form-data', 
-            'Authorization': 'Bearer AdminTest|1557462690|KURwQrTWnbMLFGz7tbiQfMhomJB4aLGA79ZBk2MVRAY|f3f36aa67c608ac5f734da8389a678f5361db1a841d9ca3f6a1f4353dcf2d68c'
+            'Authorization': 'Bearer '+localStorage.cookie
         }
     }
 
