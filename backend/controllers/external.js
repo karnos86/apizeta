719903655507e0b38d44130446a464ca2c3a94db
ronald <ronald.penaloza08@gmail.com>
2019-05-27 09:58:42 -0430

    avance

--- backend/controllers/external.js
+++ backend/controllers/external.js
@@ -233,7 +233,6 @@ module.exports={
         try{
             var data = await asyn_request('https://www.googleapis.com/youtube/v3/playlistItems?key=AIzaSyBYYx0g6DgwDeZYVM4dDuW9hq4b-VlIiYA&playlistId=UU-NHHkALnId41yENpdCuLKQ&part=snippet,id&maxResults=12',{method: 'GET'})
             var json = JSON.parse(data.body)
-            console.log(json["items"])
             res.json(json["items"]);
         }catch(error){
             res.status(500).json(error)
@@ -259,17 +258,7 @@ module.exports={
           console.log(error)
           res.status(500).json(error)
         }
-    },
-    async emailForm(req, res){
-        try {
-            var results = await transporter.loadTemplate('charge_paid', 'ronald')
-            console.log(results)
-            req.json(results)
-        } catch (error) {
-            console.log('error:',error)
-            results.json(error)
-        }
-    }
+    } 
  
 }
 
@@ -293,5 +282,28 @@ async function CalculeTimeSubcription(type, start){
     }    
 }
 
+async function bodyEmail(from, data){
+    try{
+        let mailOptions = {
+            from: from,
+            to: process.env.USER_MAIL,
+            subject: data.subect,
+            html: '<h1>Hola mundo!</h1>' 
+        };
+    }catch(error){
+        return error
+    }
+}
 
+
+// 
+
+//         transporter.sendMail(mailOptions, (error, info) => {
+//             if (error) {
+//                 console.log('Error', error);
+//             }
+//             else{
+//                 console.log('Success', info);
+//             }
+//         });
 
--- bin/conexion_mailer.js
+++ bin/conexion_mailer.js
@@ -1,36 +1,18 @@
 var nodemailer = require('nodemailer');
 var smtpTransport = require("nodemailer-smtp-transport");
-const EmailTemplate = require('email-templates').EmailTemplate,
-    path = require('path'),
-    Promise = require('bluebird');
- module.exports = {
-    start(){
-        return nodemailer.createTransport(smtpTransport({
-            service : process.env.HOST_MAIL,
-            port:  process.env.PORT_MAIL,
-            secure: false,
-            requireTLS: true, //Force TLS
-            tls: {  
-                rejectUnauthorized: false
-            },
-            auth : {
-                user : process.env.USER_MAIL,
-                pass : process.env.PASS_MAIL
-            }
-        }))
+
+module.exports = nodemailer.createTransport(smtpTransport({
+	// pool: true,
+    service : process.env.HOST_MAIL,
+    port:  process.env.PORT_MAIL,
+    secure: false,
+    requireTLS: true, //Force TLS
+    tls: {  
+        rejectUnauthorized: false
     },
-    loadTemplate (templateName, contexts) {
-        return EmailTemplate(path.join(__dirname, 'backend/emails/', templateName));
-        // return Promise.all(contexts.map((context) => {
-        //     return new Promise((resolve, reject) => {
-        //         template.render(context, (err, result) => {
-        //             if (err) reject(err);
-        //             else resolve({
-        //                 email: result,
-        //                 context,
-        //             });
-        //         });
-        //     });
-        // }));
+    auth : {
+        user : process.env.USER_MAIL,
+        pass : process.env.PASS_MAIL
     }
- } 
+   
+}));
