c17aab9396f9efde23bb56d3720081bbe88a4d6a
ronald <ronald.penaloza08@gmail.com>
2019-06-23 18:00:57 -0430

    avance

--- backend/controllers/customer.js
+++ backend/controllers/customer.js
@@ -188,6 +188,7 @@ module.exports={
         try{
             let UUII = req.headers["authorization"]
             let auth = req.body.token
+            delete req.body.token;
             let result = await Access.findOne({where:{uuii:UUII}})
             let data = req.body
             let wordpress = await asyn_request('https://zetatijuana.com/wp-json/wp/v2/users/'+result.idWordPress,{method: 'POST', data:data,
