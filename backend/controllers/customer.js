177a2d0559c5cade4fe437e1a85d5b46296a2871
Ronald Penaloza <ronald.penaloza08@gmail.com>
2019-09-01 11:25:03 -0400

    actualizar pago

--- backend/controllers/customer.js
+++ backend/controllers/customer.js
@@ -58,8 +58,10 @@ module.exports={
                 } 
             }else{
                 let customer_conekta = await conekta.Customer.find(customer["idConeckt"]);
+
                 console.log(customer_conekta );
-                await customer_conekta.subscription.update({payment_sources: data.payment_sources})
+                let payment = await customer_conekta.createPaymentSource({card: data.payment_sources});
+                console.log("pago",payment)
                 console.log(customer_conekta)
                 res.status(200).send(customer_conekta);
             }
